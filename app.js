9ba7f45b54789ce2cb69ca523271834d4b29c0a7
Ahmet Tunç Aras <74108753+Latth@users.noreply.github.com>
2022-01-19 19:00:22 +0300

    general changes about js and HTML

--- app.js
+++ app.js
@@ -17,35 +17,59 @@ function createNewTodo(text) {
     todoText.innerText = text;
     todoDiv.appendChild(todoText);
 
+    const deleteItem = document.createElement('span');
+    deleteItem.className = "deleteItem";
+    todoDiv.appendChild(deleteItem)
+
     allTodos.appendChild(todoDiv);
     totalItems++
 
-    document.getElementById('left-length').innerText = `${totalItems} items left`;
+    document.getElementById('left-length').innerText = document.getElementsByClassName('todo-item').length + " items left";
+
+    document.getElementById('newTodoText').value = "";
 }
 
 
 window.addEventListener('keyup', function(e){
-    const newTodoText = document.getElementById('newTodoText').value;
+    let newTodoText = document.getElementById('newTodoText').value;
 
     key = "Enter";
     if(e.key == key){
         if(newTodoText != ""){
-            createNewTodo(newTodoText)
+            createNewTodo(newTodoText);
         }  
     }
 
+})
+
+//Remove items from the
+
+window.addEventListener('click', function(e){
+    if(e.target.classList == "deleteItem"){
+        e.target.parentElement.remove();
+        document.getElementById('left-length').innerText = document.getElementsByClassName('todo-item').length + " items left";
+    }
 })
 
 // Task done Add Active
 window.addEventListener('click', function(e) {
 
     if(e.target.classList == "todoDone"){
-        e.target.parentElement.classList.add('active');
-        console.log(e.target.parentElement.classList)
+        e.target.parentElement.classList.add('completed');
+        e.target.classList.add('completed')
     }
+})
 
-   
+// Task done remove active element
 
-    
+window.addEventListener('mousedown', function(e) {  
+    let todoItem = document.getElementsByClassName('completed');
+    let todoItemDone = document.getElementsByClassName('todoDone');
+    console.log(e.target)
+    if(e.target.classList == "todoDone completed")
+    if(todoItem.length != 0){
+        e.target.parentElement.classList.remove('completed');
+        e.target.classList.remove('completed');
+    }
 })
 
